57b7b2447d117033e7cbe88ae3846f2945cc5c67
miura <miura@qu-bic.jp>
2020-03-28 16:49:50 +0900

    css微調整（pc、sp）

--- js/myscript.js
+++ js/myscript.js
@@ -6,13 +6,13 @@ const spwidth = 767;
 const tabletwidth = 1025;
 
 {
-	const openMenu = document.getElementById('menu_open');
-	const Nav = document.querySelector('header nav');
+	// const openMenu = document.getElementById('menu_open');
+	// const Nav = document.querySelector('header nav');
 
-	openMenu.addEventListener('click', function () {
-		openMenu.classList.toggle('active');
-		Nav.classList.toggle('active');
-	});
+	// openMenu.addEventListener('click', function () {
+	// 	openMenu.classList.toggle('active');
+	// 	Nav.classList.toggle('active');
+	// });
 }
 
 
